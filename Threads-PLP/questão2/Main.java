e213ab4c31190f5de390003bd021dec1d6fa540d
Adbys José Vasconcelos de Andrade <adbys.andrade@ccc.ufcg.edu.br>
2017-08-25 16:07:31 -0300

    threads questao 2

---
+++ Threads-PLP/questão2/Main.java
@@ -0,0 +1,13 @@
+package questão2;
+
+public class Main {
+
+	public	static	void main(String[] args)	{
+		Ponte p	= new Ponte();
+		Thread t = new Thread(p);
+		t.start();
+		p.carro("A");
+		p.carro("B");
+		p.carro("A");
+	}
+}
---
+++ Threads-PLP/questão2/Ponte.java
@@ -0,0 +1,54 @@
+package questão2;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ponte implements Runnable {
+
+	private List<String> fila;
+	
+	public Ponte() {
+		this.fila = new ArrayList<>();
+	}
+	
+	
+	public void carro(String carro) {
+		fila.add(carro);
+	}
+
+	@Override
+	public void run() {
+		String primeiro = this.fila.get(0);
+		for (int i = 0; i < fila.size(); i++) {
+			if (this.fila.contains(primeiro)) {
+				if(primeiro.equals("A"))
+					System.out.println("Passando carro A --> B");
+				else
+					System.out.println("Passando carro B --> A");
+				this.fila.remove(primeiro);
+			} 
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		if(!this.fila.isEmpty()) {
+			primeiro = this.fila.get(0);
+			while(!this.fila.isEmpty()) {
+				if (this.fila.contains(primeiro)) {
+					if(primeiro.equals("A"))
+						System.out.println("Passando carro A --> B");
+					else
+						System.out.println("Passando carro B --> A");
+					this.fila.remove(primeiro);
+				}
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+	}
+
+}
